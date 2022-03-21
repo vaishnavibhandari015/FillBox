4625995ce07a713d1cd6f68e70b5a385ffb98096
vaishnavibhandari <vaishnavibhandari39@gmail.com>
2022-03-21 16:46:32 +0530

    fill the box

---
+++ fillBox.js
@@ -0,0 +1,15 @@
+function red(){
+     document.getElementById('all').style.backgroundColor = "red";
+        }
+ function blue(){
+     document.getElementById('all').style.backgroundColor = "blue";
+        }
+function yellow(){
+    document.getElementById('all').style.backgroundColor = "yellow";
+        }
+function white(){
+     document.getElementById('all').style.backgroundColor = "white";
+        }
+function violet(){
+     document.getElementById('all').style.backgroundColor = "violet";
+        }
